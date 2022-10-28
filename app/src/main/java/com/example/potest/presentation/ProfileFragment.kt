707a59abd258717b909a4752416273716c6cb258
Nursultan <nur-sultan17@mail.ru>
2022-10-29 01:31:54 +0600

    impl ProfileFragment.kt layout

--- app/src/main/java/com/example/potest/presentation/ProfileFragment.kt
+++ app/src/main/java/com/example/potest/presentation/ProfileFragment.kt
@@ -1,10 +1,37 @@
 package com.example.potest.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.potest.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding: FragmentProfileBinding
+        get() = _binding ?: throw RuntimeException("FragmentProfileBinding is null")
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory(requireActivity().application)
+        )[MainViewModel::class.java]
+    }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 
     companion object {
         fun newInstance(): ProfileFragment {
