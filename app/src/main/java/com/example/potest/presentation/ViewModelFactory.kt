79bf3b51ae403547d80415fbd1698fadf31ebe5c
Nursultan <nur-sultan17@mail.ru>
2022-11-15 18:21:33 +0600

    implemented provider in ViewModelFactory.kt

--- app/src/main/java/com/example/potest/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/potest/presentation/ViewModelFactory.kt
@@ -2,13 +2,16 @@ package com.example.potest.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.potest.di.ApplicationScope
 import javax.inject.Inject
+import javax.inject.Provider
 
 @Suppress("UNCHECKED_CAST")
+@ApplicationScope
 class ViewModelFactory @Inject constructor(
-    private val viewModels: @JvmSuppressWildcards Map<String, ViewModel>
+    private val viewModels: @JvmSuppressWildcards Map<String, Provider<ViewModel>>
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return viewModels[modelClass.simpleName] as T
+        return viewModels[modelClass.simpleName]?.get() as T
     }
 }
